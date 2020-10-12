d93db3bd4a8fc4770a1f3ff6f6c748d522ee77a3
Alumno <cesarrgomez@hotmail.com>
2020-10-12 14:19:27 -0700

    Initial Commit

---
+++ Imagenesnavegacion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Imagenesnavegacion
+//
+//  Created by Alumno on 10/12/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
